c0425da2bba932bc107dcacbb4f8ce5fb8a94013
k1enn <dinhtrungkien05@gmail.com>
2025-03-07 10:07:29 +0700

    Fixed display wrong in lesson list

--- app/src/main/java/com/example/elsa_speak_clone/MainActivity.java
+++ app/src/main/java/com/example/elsa_speak_clone/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
     Context context;
     SharedPreferences prefs;
     private BottomNavigationView bottomNavigationView;
-    private int currentPosition = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         initializeSharedPreferences();
 
         bottomNavigationView.setSelectedItemId(R.id.nav_home);
-        loadFragment(new HomeFragment(), currentPosition);
+        loadFragment(new HomeFragment());
 
     }
     @Override
@@ -61,47 +60,24 @@ public class MainActivity extends AppCompatActivity {
 
     private final NavigationBarView.OnItemSelectedListener navListener = item -> {
         Fragment selectedFragment;
-        int newPosition;
-
         int itemId = item.getItemId();
 
         if (itemId == R.id.nav_learn) {
             selectedFragment = new LearnFragment();
-            newPosition = 0;
         } else if (itemId == R.id.nav_home) {
             selectedFragment = new HomeFragment();
-            newPosition = 1;
         } else if (itemId == R.id.nav_profile) {
             selectedFragment = new ProfileFragment();
-            newPosition = 2;
         } else {
             return false;
         }
 
-        loadFragment(selectedFragment, newPosition);
+        loadFragment(selectedFragment);
         return true;
     };
 
-    private void loadFragment(Fragment fragment, int newPosition) {
+    private void loadFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-
-        if (newPosition > currentPosition) {
-            // Swipe left animation (new fragment enters from right)
-            transaction.setCustomAnimations(
-                R.anim.slide_in_right,
-                R.anim.slide_out_left
-
-            );
-        } else if (newPosition < currentPosition) {
-            // Swipe right animation (new fragment enters from left)
-            transaction.setCustomAnimations(
-                R.anim.slide_in_left,
-                R.anim.slide_out_right
-            );
-        }
-        
         transaction.replace(R.id.fragment_container, fragment).commit();
-        
-        currentPosition = newPosition; // update position after loading fragment
     }
 }
